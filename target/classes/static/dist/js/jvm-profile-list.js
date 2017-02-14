1c5e166007ddbf48e56387a1b956d6c05df92d4c
miludedeng <383252209@qq.com>
2017-02-14 23:15:22 +0800

    add web page

---
+++ target/classes/static/dist/js/jvm-profile-list.js
@@ -0,0 +1,39 @@
+/*!
+ * Start Bootstrap - SB Admin 2 v3.3.7+1 (http://startbootstrap.com/template-overviews/sb-admin-2)
+ * Copyright 2013-2017 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ */
+$(function() {
+    var serverBasic = 'http://localhost:8080';
+
+    var trTemplate = '<tr class="odd gradeX">\n\
+                            <td>{{Method}}</td>\n\
+                            <td>{{Precent}}%</td>\n\
+                            <td>{{Time}}s</td>\n\
+                        </tr>';
+    var listCount = 25;
+    var freshProfileList = function(data) {
+        $("#profile-list").html("");
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].error) {
+                continue;
+            }
+            var temp = trTemplate.replace(/{{Method}}/g, data[i].Method).replace(/{{Precent}}/g, data[i].Precent.toFixed(2)).replace(/{{Time}}/g, data[i].Time.toFixed(2));
+            $("#profile-list").append(temp);
+        }
+    }
+
+    setInterval(function() {
+        if (!$("#profile-panel").is(':visible')) {
+            return;
+        }
+        $.get(serverBasic + '/vm_profile_list/' + $("#vm-id").val() + "/" + listCount, function(data) {
+            freshProfileList(eval("(" + data + ")"));
+        });
+    }, 1000);
+
+    $(".profile-list-count").change(function() {
+        listCount = $(this).val();
+    });
+
+});
---
+++ target/classes/static/js/jvm-thread-list.js
@@ -0,0 +1,48 @@
+$(function() {
+    var serverBasic = 'http://localhost:8080';
+
+    var trTemplate = '<tr class="odd gradeX">\n\
+                            <td>{{TID}}</td>\n\
+                            <td>{{Name}}</td>\n\
+                            <td>{{State}}</td>\n\
+                            <td>{{CPU}}%</td>\n\
+                            <td>{{TotalCPU}}%</td>\n\
+                            <td>{{BlockedBy}}</td>\n\
+                        </tr>';
+    var listCount = 25;
+    var freshThreadList = function(data) {
+        $("#thread-list").html("");
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].error) {
+                continue;
+            }
+            var temp = trTemplate.replace(/{{TID}}/g, data[i].TID).replace(/{{Name}}/g, data[i].Name).replace(/{{State}}/g, data[i].State).replace(/{{CPU}}/g, data[i].CPU.toFixed(2)).replace(/{{TotalCPU}}/g, data[i].TotalCPU.toFixed(2)).replace(/{{BlockedBy}}/g, data[i].BlockedBy);
+            $("#thread-list").append(temp);
+        }
+    }
+
+    var freshThreadCount = function(data) {
+        if (data.error) {
+            return false;
+        }
+        var count = data.ThreadCount;
+        $("#thread-count").text(count);
+    }
+
+    setInterval(function() {
+        if (!$("#thread-panel").is(':visible')) {
+            return;
+        }
+        $.get(serverBasic + '/vm_thread_list/' + $("#vm-id").val() + "/" + listCount, function(data) {
+            freshThreadList(eval("(" + data + ")"));
+        });
+        $.get(serverBasic + '/vm_thread_count/' + $("#vm-id").val(), function(data) {
+            freshThreadCount(eval("(" + data + ")"));
+        });
+    }, 1000);
+
+    $(".thread-list-count").change(function() {
+        listCount = $(this).val();
+    });
+
+});
