f5d30382ece05fb978563657134fd5bc058094a3
huwei <hw@zving.com>
2017-03-07 19:56:51 +0800

    add broad cast

--- src/main/java/cc/cafetime/info/VmJdbcInfo.java
+++ src/main/java/cc/cafetime/info/VmJdbcInfo.java
@@ -2,6 +2,7 @@ package cc.cafetime.info;
 
 import cc.cafetime.LimitQueue;
 import cc.cafetime.entity.SqlData;
+import com.jvmtop.monitor.VMInfo;
 import com.sun.tools.attach.VirtualMachine;
 
 import java.util.*;
@@ -11,12 +12,15 @@ import java.util.*;
  */
 public class VmJdbcInfo {
 
+    private static final String SQL_DATA_SPERA = "__JVM_MONITOR__";
     public final static String SQL_STAT_AGENT_LOADED = "cc.cafetime.sqlstat.loaded";
     private static Map<Integer, String> tokenMap = new HashMap<Integer, String>();
-    public static Queue<SqlData> sqlDataQueue = new LimitQueue<SqlData>(20000);
+    public static Map<String, Queue<SqlData>> sqlDataQueueMap = new HashMap<String, Queue<SqlData>>();
+    public static int QUEUE_LENGTH = 20000;
 
     /**
      * 加载jar报到指定的应用
+     *
      * @param vmId
      * @param listenAddr
      * @throws Exception
@@ -30,5 +34,32 @@ public class VmJdbcInfo {
         }
         String token = UUID.randomUUID().toString();
         vm.loadAgent("sqlstat.jar", listenAddr + "," + token);
+    }
+
+    /**
+     * 存储接收的sql信息
+     */
+    public static void saveSqlData(String recvData) {
+        String[] datas = recvData.split(SQL_DATA_SPERA);
+        if (datas.length < 4) {
+            return;
+        }
+        String vmId = datas[0];
+        if (!VmListInfo.vmInfoMap.keySet().contains(Integer.parseInt(vmId))) {
+            return;
+        }
+        SqlData sqlData = new SqlData();
+        sqlData.setCost(Long.parseLong(datas[1]));
+        sqlData.setSql(datas[2]);
+        Date date = new Date();
+        date.setTime(Long.parseLong(datas[3]));
+        sqlData.setDate(date);
+        Queue<SqlData> q = VmJdbcInfo.sqlDataQueueMap.get(vmId);
+        if (q == null) {
+            q = new LimitQueue<SqlData>(VmJdbcInfo.QUEUE_LENGTH);
+        }
+        q.offer(sqlData);
+        System.out.println("VmID: " + vmId + " Time: " + sqlData.getCost() + " SQL: " + sqlData.getSql());
+        VmJdbcInfo.sqlDataQueueMap.put(vmId, q);
     }
 }
---
+++ src/main/java/cc/cafetime/util/NetUtil.java
@@ -0,0 +1,42 @@
+package cc.cafetime.util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by steven on 2017/3/7.
+ */
+public class NetUtil {
+
+    /***
+     *  true:already in using  false:not using
+     * @param port
+     */
+    public static boolean isLoclePortUsing(int port){
+        boolean flag = true;
+        try {
+            flag = isPortUsing("127.0.0.1", port);
+        } catch (Exception e) {
+        }
+        return flag;
+    }
+    /***
+     *  true:already in using  false:not using
+     * @param host
+     * @param port
+     * @throws UnknownHostException
+     */
+    public static boolean isPortUsing(String host,int port) throws UnknownHostException {
+        boolean flag = false;
+        InetAddress theAddress = InetAddress.getByName(host);
+        try {
+            Socket socket = new Socket(theAddress,port);
+            flag = true;
+        } catch (IOException e) {
+
+        }
+        return flag;
+    }
+}
